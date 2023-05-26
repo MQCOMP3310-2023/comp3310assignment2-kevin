2c217fb96369fd2841f1dcb9cc08ecdb154898d4
stewwratt <92232004+stewwratt@users.noreply.github.com>
2023-05-26 11:39:45 +1000

    attempting to fix bandit B105 linting vulnerability by adding dotenv. CWE-259.

--- project/__init__.py
+++ project/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from dotenv import load_dotenv
+import os
 # init SQLAlchemy so we can use it later in our models
+
 db = SQLAlchemy()
 
+
 def create_app():
+    
+    load_dotenv()
+
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secret-key-do-not-reveal'
+    app.config['SECRET_KEY'] = os.getenv(SECRET_KEY)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurantmenu.db'
 
     db.init_app(app)
