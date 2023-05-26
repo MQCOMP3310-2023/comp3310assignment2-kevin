1932ad5c5460f4a971f8c497024272c884051698
stewwratt <92232004+stewwratt@users.noreply.github.com>
2023-05-26 13:53:08 +1000

    fixed B104: harcoded_bind_all_interfaces bandit vulnerability by changing to run always on local machine at the moment

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from project import create_app
 
 if __name__ == '__main__':
   app = create_app()
-  app.run(host = '0.0.0.0', port = 8000, debug=False) #Changed boolean value of debug from True to False to disable debugging
+  app.run(host = '127.0.0.1', port = 8000, debug=False) #Changed boolean value of debug from True to False to disable debugging (addition js: added host to run on local machine 127.0.0.1 fix CWE-605)
